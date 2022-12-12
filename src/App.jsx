ccbc8c19437ea8131720bed441d062e7f1f812ba
Tomasz Bielecki <szkoleniegoit@gmail.com>
2022-12-12 15:45:02 +0100

    bugfix App

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,3 @@
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//       React homework template
-//     </div>
-//   );
-// };
 import Profile from "./components/Profile";
 import user from "./data/user.json";
 import Statistics from "components/Statistics";
